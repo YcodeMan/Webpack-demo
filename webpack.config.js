6ebd2444075b150c17ed3f57aa20fdd1b51472d0
YcodeMan <1130578809@qq.com>
2019-04-14 11:51:39 +0800

    add css modules

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,22 @@ module.exports = {
 				test: /(\.jsx|\.js)$/,
 				use: {
 					loader: "babel-loader",
-					options: {
-						presets: [
-							"env", "react"
-						]
-					}
 				},
 				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: "style-loader"
+					}, {
+						loader: "css-loader",
+						options: {
+							modules: true,
+							localIdentName: '[name]__[local]--[hash:base64:5]'
+						}
+					}
+				]
 			}
 		]
 	}
